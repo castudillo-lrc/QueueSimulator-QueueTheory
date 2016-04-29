96b08c691c702246ae61bf5d3491a436c5254259
Enrique Enciso <encisoenrique@gamebank.co>
2016-04-29 05:12:32 -0500

    finish animation

--- app/scripts/animations.js
+++ app/scripts/animations.js
@@ -2,15 +2,19 @@
 var arrServers = [0,0,0,0];
 var id = 1;
 var pos=0;
-var cantidad=0;
+var quantity=0;
 var queue = [];
-
 var lambda = 3;
+var miu = 2 * 500;
+var exponential = [];
+
+$("#queueSize").text(quantity);
 
 var lapses = []
 function setLapses (laps) {
 	var r = new Rands();
 	lapses = r.poisson(lambda,[laps]);
+	exponential = r.exponential(miu, [1000])
 	for (var i = 0; i < lapses.length; i++) {
 		if(lapses[i] ==0){
 			lapses[i] = 1;
@@ -25,8 +29,8 @@ function setLapses (laps) {
 function sendClients(){
 	setInterval(function myVar(){
 
-		cantidad += lapses[pos];
-		//console.log(cantidad);
+		
+		//console.log(quantity);
 		for (var i = 0; i < lapses[pos]; i++) {
 		(function  (ins) {	
 			setTimeout(function  () {
@@ -34,7 +38,8 @@ function sendClients(){
 				moveClient("#example-"+id,0, 200);
 				moveClient("#example-"+id,350, 200);
 				queue.push("#example-"+id);
-				//console.log(queue.shift());
+				quantity += 1;
+				$("#queueSize").text(quantity);
 				id++;
 			},5000/ins)
 		})(i);
@@ -44,76 +49,84 @@ function sendClients(){
 }
 
 function path1(idS){
-	cantidad -=1;
+	//quantity -=1;
 	moveClient(idS,575, 40);
 	arrServers[0]=1;
-	ereaseClient(idS, 0);
+	ereaseClient(idS, 0, 40);
 }
 function path2(idS){
-	cantidad -=1;
+	//quantity -=1;
 	moveClient(idS,575, 145);
 	arrServers[1]=1;
-	ereaseClient(idS, 1);
+	ereaseClient(idS, 1, 145);
 }
 function path3(idS){
-	cantidad -=1;
+	//quantity -=1;
 	moveClient(idS,575, 250);
 	arrServers[2]=1;
-	ereaseClient(idS, 2);
+	ereaseClient(idS, 2, 250);
 }
 function path4(idS){
-	cantidad -=1;
+	//quantity -=1;
 	moveClient(idS,575, 350);
 	arrServers[3]=1;
-	ereaseClient(idS, 3);
+	ereaseClient(idS, 3, 350);
 }
 
-function ereaseClient(ids, position){
+function ereaseClient(ids, position, y){
+	quantity-=1;
+	$("#queueSize").text(quantity);
 	setTimeout(function  () {
+		
+		moveClient(ids,600, y);
 				console.log("borrando "+ids);
 				$( ids ).remove();
 				arrServers[position]=0;
-				console.log(arrServers[position]);
+				//console.log(arrServers[position]);
+				
+				console.log("borrado "+exponential.shift());
 				;
-			},5000)
+			}, (1/exponential.shift())+5000)
+
 }
 
 function printClients(){
 	setInterval(function myVar(){
+		console.log(quantity);
 		//console.log(queue.shift());
 		var random = Math.random();
 		//console.log(arrServers);
 		if($.inArray(0,arrServers) != -1){
-			if(arrServers[0]==0 && random < 1/4){
-				path1(queue.shift());
+
+			if(queue[0] !== 'undefined'){
+				try{
+					//console.log("moviendo al servidor");
+					if(arrServers[0]==0 && random < 1/4){
+						path1(queue.shift());
+					}
+					else if(arrServers[1]==0 && random >= 1/4 && random < 2/4){
+						path2(queue.shift());
+					}
+					else if(arrServers[2]==0 && random >= 2/4 && random < 3/4){
+						path3(queue.shift());
+					}
+					else if(arrServers[3]==0 && random >= 3/4 && random < 4/4){
+						path4(queue.shift());	
+					}
+				}catch(err){
+					//console.log("there is no people in the queue");
+				}
+				//console.log("funciona");
 			}
-			else if(arrServers[1]==0 && random >= 1/4 && random < 2/4){
-				path2(queue.shift());
-			}
-			else if(arrServers[2]==0 && random >= 2/4 && random < 3/4){
-				path3(queue.shift());
-			}
-			else if(arrServers[3]==0 && random >= 3/4 && random < 4/4){
-				path4(queue.shift());	
-			}
-			//console.log("funciona");
 		}
 	}, 100)
 }
 
 //console.log(clients);
 
-document.getElementById('playButton').onclick = function(e) {
-  /*move('#example-1')
-    .to(500, 200)
-    .end();*/
-
-    $( "#canvas" ).append( '<div id="example-'+1+'" class="circle small"></div>');
-    moveClient("#example-1", 100, 200);
-    moveClient("#example-1", 350, 200);
-
-};
-
+document.getElementById('playButton').onclick = function (){
+	init();
+}
 
 
 function moveClient(id, x, y){
@@ -144,3 +157,13 @@ function getXpos (id) {
 	console.log(arr);
 	console.log(val);
 }
+
+
+
+function init() {
+	console.log("initiating simulation");
+	setLapses(24);
+	sendClients();
+	printClients();
+}
+
